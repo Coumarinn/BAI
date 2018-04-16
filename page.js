559eaf3372e8239b7be3947b82dbc50d41172dce
Mistblinde <pawelboka@interia.pl>
2018-04-16 19:00:41 +0200

    added facebook login button (app page needed later)

--- page.js
+++ page.js
@@ -60,6 +60,26 @@ function googleSignin() {
     });
 }
 
+var provider = new firebase.auth.FacebookAuthProvider();
+
+function facebookLogin() {
+    firebase.auth()
+
+        .signInWithPopup(provider).then(function (result) {
+        var token = result.credential.accessToken;
+        var user = result.user;
+
+        console.log(token)
+        console.log(user)
+    }).catch(function (error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        console.log(error.code)
+        console.log(error.message)
+    });
+}
+
 function register() {
     var userEmail = document.getElementById("email_field").value;
     var userPass = document.getElementById("password_field").value;
