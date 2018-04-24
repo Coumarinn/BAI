f1314c9592ee7ebde36c7b93a288255c1d50effa
Mistblinde <pawelboka@interia.pl>
2018-04-24 15:43:06 +0200

    dodano rejestracje

--- register/register.js
+++ register/register.js
@@ -5,6 +5,17 @@ function signInByGoogle(){
 }
 
 function createAccount() {
+    var userEmail = document.getElementById("inputEmail").value;
+    var userPass = document.getElementById("inputPassword").value;
+
+    firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+
+        window.alert("Error : " + errorMessage);
+    });
 }
 
 function logIn(){
