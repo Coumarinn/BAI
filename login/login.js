26c4a2dba6f1f2abb927babbe81e61f16d551f9d
Mistblinde <pawelboka@interia.pl>
2018-04-24 15:42:36 +0200

    dodano logowanie poprzez facebooka i google

--- login/login.js
+++ login/login.js
@@ -9,7 +9,37 @@ function logIn(){
 }
 
 function logInByFacebook(){
+    firebase.auth()
+
+        .signInWithPopup(provider).then(function (result) {
+        var token = result.credential.accessToken;
+        var user = result.user;
+
+        console.log(token)
+        console.log(user)
+    }).catch(function (error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        console.log(error.code)
+        console.log(error.message)
+    });
 }
 
 function logInByGoogle(){
+    firebase.auth()
+
+        .signInWithPopup(provider).then(function (result) {
+        var token = result.credential.accessToken;
+        var user = result.user;
+
+        console.log(token)
+        console.log(user)
+    }).catch(function (error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        console.log(error.code)
+        console.log(error.message)
+    });
 }
