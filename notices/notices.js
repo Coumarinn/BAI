267397b4f3e0aaec5f75d6b0a11b564e1aea135e
Wojciech Panek <panekwojciech@gmail.com>
2018-04-25 08:41:38 +0200

    wczytywanie ogloszen z firebase

--- notices/notices.js
+++ notices/notices.js
@@ -23,3 +23,40 @@ function join(){
 function cancel(){
     console.log("anuluj");
 }
+
+function notesFromFB(place, time, description){
+  var frag = document.getElementById("notices");
+
+  var elem = document.createElement('div');
+  elem.innerHTML = '<ul class="list-group col-md-6 d-inline-block" style="padding-bottom: 20px">\
+    <li class="list-group-item active">Uzytkownik, ktory stworzyl</li>\
+    <li class="list-group-item"><b>Miejsce:</b>'+ place +'</li>\
+    <li class="list-group-item"><b>Data:</b>' + time +'</li>\
+    <li class="list-group-item"><b>Opis: ...</b>' + description +'</li>\
+    <li class="list-group-item">\
+        <button type="button" class="btn btn-outline-primary col-md-4">Dołącz</button>\
+        <button type="button" class="btn btn-outline-secondary col-md-4 d-none">Anuluj</button>\
+    </li>\
+  </ul>';
+
+  while (elem.childNodes[0]) {
+        frag.appendChild(elem.childNodes[0]);
+    }
+    return frag;
+}
+
+
+function addFromFB(){
+  var database = firebase.database().ref().child("notes");
+  var frag = document.getElementById("notices");
+
+  while (frag.firstChild) {
+    frag.removeChild(frag.firstChild);
+  }
+  database.once("value", function(snapshot) {
+    snapshot.forEach(function(child) {
+      //console.log(child.key+": "+child.child("place").val());
+      notesFromFB(child.child("place").val(), child.child("time").val(), child.child("description").val());
+    });
+  });
+}
