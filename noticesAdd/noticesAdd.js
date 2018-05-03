dd38146d6b7b150834c3856a0202bdaad802f096
Mistblinde <pawelboka@interia.pl>
2018-05-03 09:02:35 +0200

    zapisywanie w baziie firebase szerokosci i dlugosci geogr

--- noticesAdd/noticesAdd.js
+++ noticesAdd/noticesAdd.js
@@ -9,44 +9,50 @@ function initialize() {
         document.getElementById('Lat').value = place1.geometry.location.lat();
         document.getElementById('Lng').value = place1.geometry.location.lng();
 //
-        window.alert(place1.geometry.location.lat());
-        window.alert(place1.geometry.location.lng());
+        latitude = place1.geometry.location.lat();
+        longitude = place1.geometry.location.lng();
+
+        window.alert(latitude);
+        window.alert(longitude);
 
     });
 }
+
 google.maps.event.addDomListener(window, 'load', initialize);
 
-function pushData(){
+function pushData() {
     var place = $("#place").val();
     var date = document.getElementsByName("date")[0].value
     var time = document.getElementsByName("time")[0].value
     var description = $("#description").val();
-    var userId =   firebase.auth().currentUser.uid;
+    var userId = firebase.auth().currentUser.uid;
     var userEmail = firebase.auth().currentUser.email;
     var createdAt = firebase.database.ServerValue.TIMESTAMP;
 
     if ((place == '' || place == undefined) ||
-       (time == '' || time == undefined)  ||
-       (date == '' || date == undefined) ) {
-      window.alert("Uzupelnij pole miejsce i czas");
+        (time == '' || time == undefined) ||
+        (date == '' || date == undefined)) {
+        window.alert("Uzupelnij pole miejsce i czas");
     } else {
-      var firebaseRef = firebase.database().ref();
+        var firebaseRef = firebase.database().ref();
 
-      var dataToPush = {
-        userId: userId,
-        userEmail: userEmail,
-        place: place,
-        date: date,
-        time: time,
-        description: description,
-        createdAt: createdAt
-      }
+        var dataToPush = {
+            userId: userId,
+            userEmail: userEmail,
+            place: place,
+            latitude: latitude,
+            longitude: longitude,
+            date: date,
+            time: time,
+            description: description,
+            createdAt: createdAt
+        }
 
-      firebaseRef.child("notes").push().set(dataToPush);
+        firebaseRef.child("notes").push().set(dataToPush);
 
-      window.alert("Zgloszenie dodane");
+        window.alert("Zgloszenie dodane");
 
-      loadMultipleHtmlFiles("noticesUsers");
+        loadMultipleHtmlFiles("noticesUsers");
 
     }
 
