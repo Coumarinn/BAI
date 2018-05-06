14e6ca75b657c0ac1a63e8df392aafe84c19ed15
Mistblinde <pawelboka@interia.pl>
2018-05-06 12:56:20 +0200

    dodano funkcje "forgot password", po kliknieciu przychodzi na podany mail link aktywacyjny do zmiany hasla; zmienono tresc maila

--- forgotPassword/forgotPassword.js
+++ forgotPassword/forgotPassword.js
@@ -1,3 +1,12 @@
 function forgotPassword() {
-    
+    var auth = firebase.auth();
+    var emailAddress = document.getElementById("inputEmail").value;
+
+    auth.sendPasswordResetEmail(emailAddress).then(function () {
+        // Email sent.
+    }).catch(function (error) {
+        // An error happened.
+        var errorMessage = error.message;
+        window.alert("Error : " + errorMessage);
+    });
 }
