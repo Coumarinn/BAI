2efb64488368936359f6d352a6419843c0b39158
panekwojciech <panekwojciech@gmail.com>
2018-05-07 20:33:52 +0200

    wyswietla ogloszenia w zaleznosci od odleglosci 5/10/20

--- notices/notices.js
+++ notices/notices.js
@@ -118,7 +118,7 @@ function anulujButton(key){
 }
 
 
-function addFromFB(){
+function addFromFB(distParam){
   var database = firebase.database().ref().child("notes");
   var frag = document.getElementById("notices");
 
@@ -126,10 +126,18 @@ function addFromFB(){
     frag.removeChild(frag.firstChild);
   }
 
+  navigator.geolocation.getCurrentPosition(function(position) {
+                lat = position.coords.latitude;
+                lng = position.coords.longitude;
+              })
+
+
   database.once("value", function(snapshot) {
     snapshot.forEach(function(child) {
       let note = child.val();
 
+      let distance = getDistanceFromLatLonInKm(lat,lng,note.lat,note.lng).toFixed(1);
+      if (distance < distParam){
       notesFromFB(note.userEmail, note.place, note.date, note.time, note.description, child.key);
 
       // wyroznienie ogloszen, ktore uzytkownik stworzyl
@@ -154,8 +162,30 @@ function addFromFB(){
         if (note.members[userId] != undefined) {
           anulujButton(key);
     }}
+  }
     });
 
 
   });
+}
+
+
+//obliczanie odleglosci miedzy wspolrzednymi
+
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1);
+  var a =
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ;
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
 }
