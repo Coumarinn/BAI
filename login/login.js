fcd7bd98dfb2df2cd54ea8dfa2349d8f0319f54c
Mistblinde <pawelboka@interia.pl>
2018-05-08 09:42:17 +0200

    signInWithRedirect - zmiana na nowa funkcje przy logowanie przez google

--- login/login.js
+++ login/login.js
@@ -50,19 +50,34 @@ function logInByFacebook(){
 
 var provider = new firebase.auth.GoogleAuthProvider();
 function logInByGoogle(){
-    firebase.auth()
 
-        .signInWithPopup(provider).then(function (result) {
+    firebase.auth().signInWithRedirect(provider).then(function() {
+        return firebase.auth().getRedirectResult();
+    }).then(function(result) {
+        // This gives you a Google Access Token.
+        // You can use it to access the Google API.
         var token = result.credential.accessToken;
+        // The signed-in user info.
         var user = result.user;
-
-        console.log(token)
-        console.log(user)
-    }).catch(function (error) {
+        // ...
+    }).catch(function(error) {
+        // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
+    });
 
-        console.log(error.code)
-        console.log(error.message)
+    firebase.auth().getRedirectResult().then(function(result) {
+        if (result.credential) {
+            // This gives you a Google Access Token.
+            // You can use it to access the Google API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            // ...
+        }
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
     });
 }
