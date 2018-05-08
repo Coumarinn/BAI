a29acd551f93ab8c25800c17fc38e29765881118
Mistblinde <pawelboka@interia.pl>
2018-05-08 16:48:30 +0200

    drobne zmiany z window.location

--- login/login.js
+++ login/login.js
@@ -20,6 +20,9 @@ function logIn(){
 
     firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
         // Handle Errors here.
+
+        window.location = '#/home';
+
         var errorCode = error.code;
         var errorMessage = error.message;
 
@@ -59,6 +62,8 @@ function logInByGoogle(){
         // The signed-in user info.
         var user = result.user;
         // ...
+
+        window.location = '#/home';
 
         console.log(token)
         console.log(user)
