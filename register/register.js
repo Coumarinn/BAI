c31d2dc831bd57216729c9c5442026d233bd3f60
Mistblinde <pawelboka@interia.pl>
2018-05-10 07:00:48 +0200

    dodanie alertu po udanym zalogowaniu

--- register/register.js
+++ register/register.js
@@ -9,6 +9,9 @@ function createAccount() {
     var userPass = document.getElementById("inputPassword").value;
 
     firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function (error) {
+
+        window.alert("Udalo sie zarejestrowac, witamy!")
+
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
