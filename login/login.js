81ecdf7c6cce7053d69a7e9a7419557d34d71a32
panekwojciech <panekwojciech@gmail.com>
2018-05-10 22:45:07 +0200

    drobna zmiana w location.replace po logowaniu

--- login/login.js
+++ login/login.js
@@ -22,7 +22,7 @@ function logIn(){
     firebase.auth().signInWithEmailAndPassword(userEmail, userPass).then(function(user) {
 
         window.alert("Udalo sie zalogowac, witamy!")
-        window.location.replace("/android_asset/www/index.html#/home");
+        window.location.replace("#/home");
     }).catch(function(error) {
         // Handle Errors here.
 
