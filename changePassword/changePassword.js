a633efd709dcaf9a4c02e3cbb3e6b772473ad3d3
Mistblinde <pawelboka@interia.pl>
2018-05-10 15:48:46 +0200

    dodanie buttonu do zmiany hasla

--- changePassword/changePassword.js
+++ changePassword/changePassword.js
@@ -1,3 +1,12 @@
 function changePassword() {
-    console.log("zmień hasło");
+    var user = firebase.auth().currentUser;
+    var newPassword = getASecureRandomPassword();
+
+    user.updatePassword(newPassword).then(function() {
+        // Update successful.
+        window.alert("Haslo zresetowane")
+    }).catch(function(error) {
+        // An error happened.
+        window.alert("Error : " + errorMessage);
+    });
 }
